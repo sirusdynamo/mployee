4f2b35a5eea0dbdaeb0681c693c2428f96ce2ffd
sirusdynamo <sirusdynamo@gmail.com>
2018-12-18 12:25:22 +0100

    Application structure defined

---
+++ config.py
@@ -0,0 +1,28 @@
+class Config(object):
+    """
+    common configurations
+    """
+
+    #put any configuration here that is common to all environments
+
+
+class DevelopmentConfig(Config):
+    """
+    Develpoment configurations
+    """
+    DEBUG =True
+    SQLALCHEMY_ECHO=True
+
+
+
+class ProductionConfig(Config):
+    """ 
+    Production configurations
+    """
+    DEBUG=False
+
+
+app_config={
+        'development':DevelopmentConfig,
+        'production':ProductionConfig
+    }
